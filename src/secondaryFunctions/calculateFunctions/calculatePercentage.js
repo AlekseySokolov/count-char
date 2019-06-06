4d5016bcc596dae384821832949110ca98f88ce8
Aleksey Sokolov <evilsokol@gmail.com>
2019-06-06 16:40:53 +0300

    refactor

--- src/secondaryFunctions/calculateFunctions/calculatePercentage.js
+++
@@ -1,9 +0,0 @@
-const calculatePercentage = (obj, totalAmount) =>
-    Object.values(obj)
-        .reduce((acc, value) =>
-                totalAmount = value / totalAmount * 100
-            ,totalAmount).toFixed(3)
-
-export default calculatePercentage;
-
-
