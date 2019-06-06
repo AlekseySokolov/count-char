b584c9c22664e48dd22537869072e3b00ca74a70
Aleksey Sokolov <evilsokol@gmail.com>
2019-06-06 13:36:50 +0300

    refactor

--- src/Components/MainContent/List/List.js
+++ src/Components/MainContent/List/List.js
@@ -2,19 +2,18 @@ import React from 'react';
 import SymbolInfo from './SymbolInfo';
 import calculatePercentage from '../../../secondaryFunctions/calculateFunctions/calculatePercentage';
 
-
 const List = (props) => {
     const {list, totalAmount} = props
     return (
         <div className='charactersList'>
             <ul className='list'>
-             {list.map((obj, index) =>
-                 <li key={index}>
-                   <SymbolInfo
-                     char={Object.keys(obj)}
-                     count={Object.values(obj)}
-                     percentOfTotal={calculatePercentage(obj, totalAmount)}/>
-                 </li>)}
+                {list.map((obj, index) =>
+                    <li key={index}>
+                        <SymbolInfo
+                            char={Object.keys(obj)[0]}
+                            count={Object.values(obj)[0]}
+                            percentOfTotal={calculatePercentage(obj, totalAmount)}/>
+                    </li>)}
             </ul>
         </div>
     )
