b56580786c362b84ca00a78ef04e5807d0e2066a
Aleksey Sokolov <evilsokol@gmail.com>
2019-06-07 15:32:41 +0300

    refactor toColl

---
+++ src/secondaryFunctions/conversionFunctions/calcPercentage.js
@@ -0,0 +1,3 @@
+const calcPercentage = (count, total) => (count / total * 100).toFixed(3);
+
+export default calcPercentage;
--- src/secondaryFunctions/conversionFunctions/toColl.js
+++ src/secondaryFunctions/conversionFunctions/toColl.js
@@ -1,20 +1,24 @@
 import filterHTML from './filterHTML';
+import calcPercentage from './calcPercentage';
 
 const toColl = (html) => {
-    const map = new Map();
-    const symbols = filterHTML(html).split('');
+    const symbols = filterHTML(html).split(''),
+          total = symbols.length,
+          data = symbols.reduce((acc, sym) => {
+            if(acc[sym] === undefined) {
+             acc[sym] = {
+              char : sym,
+              count : 1 ,
+              percentOfTotal : calcPercentage(1, total)
+            }
+            } else {
+              acc[sym].count += 1;
+              acc[sym].percentOfTotal = calcPercentage(acc[sym].count, total);
+            }
+            return acc
+          } ,{});
 
-    symbols.forEach(value => map.has(value) ?
-    map.get(value).count += 1 :
-    map.set(value, {char: value, count: 1}));
-
-    const coll = [...map.values()];
-    const totalAmount = coll.reduce((acc, obj) => acc + obj.count, 0);
-
-    coll.forEach(obj =>
-        obj.percentOfTotal = (obj.count / totalAmount * 100).toFixed(3));
-
-    return coll;
-}
+    return Object.values(data);
+};
 
 export default toColl;
